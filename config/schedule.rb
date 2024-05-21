87665135b3ebd493f312ded5a9b371acb5b6c1cf
George Pogrebnoi <gosha1416@yandex.ru>
2024-05-21 19:43:30 +0300

    Update reports sending time

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,8 +21,8 @@
 set :output, { error: '/var/log/cron_log.log', standard: '/var/log/cron_log.log' }
 job_type :custom_rake, 'cd :path && bundle install && bundle exec rake :task --silent :output'
 
-every :day, at: '12:00am' do
-  custom_rake 'reports:send'
+every :day, at: '9:00am' do
+  custom_rake "reports:send"
 end
 
 every :day, at: '6:00pm' do
